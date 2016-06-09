4cdf7d8979d11c93cfc4ce8d3920e638ff786a2a
Marcus Cavanaugh <m@mcav.com>
2016-06-09 14:00:19 -0700

    Add "created" column to the `users` table.

---
+++ migrations.sql
@@ -0,0 +1,13 @@
+-- Add lines to this file when you modify the database.
+-- Execute the appropriate statements when needed;
+
+CREATE TABLE IF NOT EXISTS users (
+    id integer NOT NULL,
+    username character(255),
+    api_key text,
+    email character(255),
+    reset_token text,
+    reset_expires timestamp without time zone
+);
+
+ALTER TABLE users ADD COLUMN created TIMESTAMP WITHOUT TIME ZONE DEFAULT NOW();
